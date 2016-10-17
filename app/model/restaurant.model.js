88ea3e79ed2067a5a0eb5623cd02fa24f772e3ac
Ruben Soler <r.solerginer@gmail.com>
2016-10-17 19:36:37 +0200

    clase modelo de restaurante

---
+++ app/model/restaurant.model.js
@@ -0,0 +1,22 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Restaurante;
+    return {
+        setters:[],
+        execute: function() {
+            Restaurante = (function () {
+                function Restaurante(id, nombre, direccion, descripcion, imagen, precio) {
+                    this.id = id;
+                    this.nombre = nombre;
+                    this.direccion = direccion;
+                    this.imagen = imagen;
+                    this.precio = precio;
+                }
+                return Restaurante;
+            }());
+            exports_1("Restaurante", Restaurante);
+        }
+    }
+});
+//# sourceMappingURL=restaurant.model.js.map
